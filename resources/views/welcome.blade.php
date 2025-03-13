3942ceee000d1fd9cb4581c28adf07860e17b149
sebgates <sebgates@gmail.com>
2025-03-13 19:19:15 +0000

    text change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,6 +125,7 @@
 
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        <h1>Hello World!</h1>
                     </div>
                 </div>
             </div>
